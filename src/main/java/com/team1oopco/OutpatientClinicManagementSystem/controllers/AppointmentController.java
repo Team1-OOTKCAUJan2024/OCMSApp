8dde655be1a39c41b40ae097eb667c311f5ad391
Kiiru-Anastasia <kiiru.anastasia@gmail.com>
2024-03-27 23:38:12 +0300

    Appointment page and add function structure(not working)

--- src/main/java/com/team1oopco/OutpatientClinicManagementSystem/controllers/AppointmentController.java
+++ src/main/java/com/team1oopco/OutpatientClinicManagementSystem/controllers/AppointmentController.java
@@ -1,13 +1,75 @@
 package com.team1oopco.OutpatientClinicManagementSystem.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.team1oopco.OutpatientClinicManagementSystem.models.AdminStaff;
+import com.team1oopco.OutpatientClinicManagementSystem.models.Appointment;
+import com.team1oopco.OutpatientClinicManagementSystem.models.Patient;
+import com.team1oopco.OutpatientClinicManagementSystem.services.AdminStaffService;
+import com.team1oopco.OutpatientClinicManagementSystem.services.AppointmentService;
+import com.team1oopco.OutpatientClinicManagementSystem.services.PatientService;
 
 @Controller
 public class AppointmentController {
 	
+	@Autowired
+	private AppointmentService appointmentService;
+	private PatientService patientService;
+	private AdminStaffService adminStaffService;
+	
 	@GetMapping("/appointments")
-	public String getPatients() {
+	public String getAppointments(Model model) {
+		
+		List<Appointment> appointmentList = appointmentService.getAppointments();
+		model.addAttribute("appointments", appointmentList);
+		
+		List<Patient> patientList = patientService.getPatients();
+		model.addAttribute("patients", patientList);
+		
+		List<AdminStaff> adminStaffList = adminStaffService.getAdminStaffs();
+		model.addAttribute("adminStaffs", adminStaffList);
+		
 		return "appointment";
+	}
+	
+	@PostMapping("appointments/addNew")
+	public String addNew(Appointment appointment) {
+		
+		appointmentService.save(appointment);
+		
+		return "redirect:/appointments";
+	}
+	
+	@RequestMapping("appointments/findById")
+	@ResponseBody
+	public Optional<Appointment> findById(Integer appointmentid) {
+		
+		return appointmentService.findById(appointmentid);
+	}
+	
+	@RequestMapping(value="appointments/update", method= {RequestMethod.PUT, RequestMethod.GET})
+	public String update(Appointment appointment) {
+		
+		appointmentService.save(appointment);
+		
+		return "redirect:/appointments";
+	}
+	
+	@RequestMapping(value="appointments/delete", method= {RequestMethod.DELETE, RequestMethod.GET})
+	public String delete(Integer appointmentid) {
+		
+		appointmentService.delete(appointmentid);
+		
+		return "redirect:/appointments";
 	}
 }
--- src/main/java/com/team1oopco/OutpatientClinicManagementSystem/services/AppointmentService.java
+++ src/main/java/com/team1oopco/OutpatientClinicManagementSystem/services/AppointmentService.java
@@ -1,6 +1,7 @@
 package com.team1oopco.OutpatientClinicManagementSystem.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,31 @@ import com.team1oopco.OutpatientClinicManagementSystem.repositories.AppointmentR
 
 @Service
 public class AppointmentService {
+	
 	@Autowired
 	private AppointmentRepository appointmentRepository;
 	
 	//returning list of appointments
 	public List<Appointment> getAppointments(){
 		return appointmentRepository.findAll();
+	}
+	
+	//returning ddl of appointments. Add query to filter today's appointments only
+	/*public List<Appointment> ddlAppointments(){
+			return appointmentRepository.findAll();
+		}*/
+	
+	//saving new appointment data
+	public void save(Appointment appointment) {
+		appointmentRepository.save(appointment);
+	}
+	
+	public void delete(Integer appointmentid) {
+		appointmentRepository.deleteById(appointmentid);
+	}
+	
+	//get by consultation id
+	public Optional<Appointment> findById(Integer appointmentid) {
+		return appointmentRepository.findById(appointmentid);
 	}
 }
