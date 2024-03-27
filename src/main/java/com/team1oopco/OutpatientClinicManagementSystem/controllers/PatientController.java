179966a4a9d5e3c96da8645cb76d03cb465cedf4
Kiiru-Anastasia <kiiru.anastasia@gmail.com>
2024-03-27 08:07:47 +0300

    Update to add new patients and return a list of patients

--- src/main/java/com/team1oopco/OutpatientClinicManagementSystem/controllers/PatientController.java
+++ src/main/java/com/team1oopco/OutpatientClinicManagementSystem/controllers/PatientController.java
@@ -1,13 +1,37 @@
 package com.team1oopco.OutpatientClinicManagementSystem.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.team1oopco.OutpatientClinicManagementSystem.models.Patient;
+import com.team1oopco.OutpatientClinicManagementSystem.services.PatientService;
 
 @Controller
 public class PatientController {
 	
+	@Autowired
+	private PatientService patientService;
+	
 	@GetMapping("/patients")
-	public String getPatients() {
+	public String getPatients(Model model) {
+		
+		List<Patient> patientList = patientService.getPatients();
+		
+		model.addAttribute("patients", patientList);
+		
 		return "patient";
+	}
+	
+	@PostMapping("/patients/addNew")
+	public String addNew(Patient patient) {
+		
+		patientService.save(patient);
+		
+		return "redirect:/patients";
 	}
 }
--- src/main/java/com/team1oopco/OutpatientClinicManagementSystem/services/PatientService.java
+++ src/main/java/com/team1oopco/OutpatientClinicManagementSystem/services/PatientService.java
@@ -1,8 +1,26 @@
 package com.team1oopco.OutpatientClinicManagementSystem.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.team1oopco.OutpatientClinicManagementSystem.models.Patient;
+import com.team1oopco.OutpatientClinicManagementSystem.repositories.PatientRepository;
 
 @Service
 public class PatientService {
-
+	
+	@Autowired
+	private PatientRepository patientRepository;
+	
+	//returning list of patients
+	public List<Patient> getPatients(){
+		return patientRepository.findAll();
+	}
+	
+	//saving new patient
+	public void save(Patient patient) {
+		patientRepository.save(patient);
+	}
 }
