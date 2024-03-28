3a3508f89102bd6848cde4ab21ae5e2445dc99b2
Kiiru-Anastasia <kiiru.anastasia@gmail.com>
2024-03-28 16:55:35 +0300

    Pharmacy requests flow

--- src/main/java/com/team1oopco/OutpatientClinicManagementSystem/controllers/PharmacyController.java
+++ src/main/java/com/team1oopco/OutpatientClinicManagementSystem/controllers/PharmacyController.java
@@ -1,13 +1,83 @@
 package com.team1oopco.OutpatientClinicManagementSystem.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.team1oopco.OutpatientClinicManagementSystem.models.Appointment;
+import com.team1oopco.OutpatientClinicManagementSystem.models.DoctorConsultation;
+import com.team1oopco.OutpatientClinicManagementSystem.models.MedicalStaff;
+import com.team1oopco.OutpatientClinicManagementSystem.models.Pharmacy;
+import com.team1oopco.OutpatientClinicManagementSystem.models.Triage;
+import com.team1oopco.OutpatientClinicManagementSystem.services.AppointmentService;
+import com.team1oopco.OutpatientClinicManagementSystem.services.DoctorConsultationService;
+import com.team1oopco.OutpatientClinicManagementSystem.services.MedicalStaffService;
+import com.team1oopco.OutpatientClinicManagementSystem.services.PharmacyService;
+import com.team1oopco.OutpatientClinicManagementSystem.services.TriageService;
 
 @Controller
 public class PharmacyController {
 	
+	@Autowired
+	private PharmacyService pharmacyService;
+	
+	@Autowired
+	private DoctorConsultationService doctorConsultationService;
+	
+	@Autowired
+	private MedicalStaffService medicalStaffService;
+	
 	@GetMapping("/pharmacy")
-	public String getPatients() {
+	public String getPharmacy(Model model) {
+		
+		List<Pharmacy> pharmacyList = pharmacyService.getPharmacy();
+		model.addAttribute("pharmacies", pharmacyList);
+		
+		List<DoctorConsultation> doctorConsultationList = doctorConsultationService.getDoctorConsultations();
+		model.addAttribute("consultations", doctorConsultationList);
+		
+		List<MedicalStaff> medicalStaffList = medicalStaffService.getMedicalStaffs();
+		model.addAttribute("medicalStaffs", medicalStaffList);
+		
 		return "pharmacy";
+	}
+	
+	@PostMapping("pharmacy/addNew")
+	public String addNew(Pharmacy pharmacy) {
+		
+		pharmacyService.save(pharmacy);
+		
+		return "redirect:/pharmacy";
+	}
+	
+	@RequestMapping("pharmacy/findById")
+	@ResponseBody
+	public Optional<Pharmacy> findById(Integer pharmacyid) {
+		
+		return pharmacyService.findById(pharmacyid);
+	}
+	
+	@RequestMapping(value="pharmacy/update", method= {RequestMethod.PUT, RequestMethod.GET})
+	public String update(Pharmacy pharmacy) {
+		
+		pharmacyService.save(pharmacy);
+		
+		return "redirect:/pharmacy";
+	}
+	
+	@RequestMapping(value="pharmacy/delete", method= {RequestMethod.DELETE, RequestMethod.GET})
+	public String delete(Integer pharmacyid) {
+		
+		pharmacyService.delete(pharmacyid);
+		
+		return "redirect:/pharmacy";
 	}
 }
--- src/main/java/com/team1oopco/OutpatientClinicManagementSystem/services/PharmacyService.java
+++ src/main/java/com/team1oopco/OutpatientClinicManagementSystem/services/PharmacyService.java
@@ -1,8 +1,35 @@
 package com.team1oopco.OutpatientClinicManagementSystem.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.team1oopco.OutpatientClinicManagementSystem.models.Pharmacy;
+import com.team1oopco.OutpatientClinicManagementSystem.repositories.PharmacyRepository;
 
 @Service
 public class PharmacyService {
-
+	@Autowired
+	private PharmacyRepository pharmacyRepository;
+	
+	//returning list of pharmacy requests
+	public List<Pharmacy> getPharmacy(){
+		return pharmacyRepository.findAll();
+	}
+	
+	//saving new pharmacy data
+	public void save(Pharmacy pharmacy) {
+		pharmacyRepository.save(pharmacy);
+	}
+	
+	public void delete(Integer pharmacyid) {
+		pharmacyRepository.deleteById(pharmacyid);
+	}
+	
+	//get by consultation id
+	public Optional<Pharmacy> findById(Integer pharmacyid) {
+		return pharmacyRepository.findById(pharmacyid);
+	}
 }
